4d136f332b809bff0b0cc7876664ba49c5c8d67e
OtokarSefer <Otokar.Sefer@voco.ee>
2025-10-19 22:48:48 +0300

    nevermind

--- server/fireBase.js
+++ server/fireBase.js
@@ -1,19 +1,8 @@
-// fireBase.js
-import admin from "firebase-admin"
-import { getFirestore } from "firebase-admin/firestore"
-import dotenv from "dotenv"
-
-dotenv.config() // Load .env variables
+import admin from 'firebase-admin';
+import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-})
+  credential: admin.credential.cert(serviceAccount)
+});
 
-const db = getFirestore()
-
-export { admin, db }
-export default admin
+export default admin;
